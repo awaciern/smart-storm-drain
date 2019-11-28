61a6f6e808c4bb2b5b14396d2e45554480293aee
awaciern <awaciern@ncsu.edu>
2019-11-27 20:00:52 -0500

    Changing STATIC_URL to azure server

--- smart_storm_drain/settings.py
+++ smart_storm_drain/settings.py
@@ -118,11 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/site/wwwroot/staticfiles/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    'D:/home/site/wwwroot/static',
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
