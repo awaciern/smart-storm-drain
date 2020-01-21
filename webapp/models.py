4f5a5a0e8f3987dcf00bc699fcdcdab3206b1ce1
awaciern <awaciern@ncsu.edu>
2020-01-20 23:56:08 -0500

    Removed commented out old Message model

--- webapp/models.py
+++ webapp/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 
-# class Message(models.Model):
-#     text = models.CharField(max_length=100)
-#     date = models.DateTimeField(default=timezone.now)
-#
-#     def __str__(self):
-#         return self.text
-
-
 class Device(models.Model):
     name = models.CharField(max_length=100)
     # location will be needed
