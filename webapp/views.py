761d782bf6920868ef61bb2d7c069dbc6ee52df6
awaciern <awaciern@ncsu.edu>
2020-01-22 00:17:10 -0500

    Removed old commented out code from views

--- webapp/views.py
+++ webapp/views.py
@@ -34,28 +34,3 @@ def index(request):
 
 def ui(request):
     return render(request, 'ui.html', context=None)
-
-
-
-# def db(request, choice):
-#     return render(request, 'ui.html', context=None)
-#
-#     if choice == 'none':
-#         messages = Message.objects.none()
-#     elif choice == 'all':
-#         messages = Message.objects.all()
-#     elif choice == 'first':
-#         earliest = Message.objects.earliest('date')
-#         messages = Message.objects.filter(date=earliest.date)
-#     elif choice == 'last':
-#         latest = Message.objects.latest('date')
-#         messages = Message.objects.filter(date=latest.date)
-#
-#     if request.method == 'POST':
-#         form = MessageForm(request.POST)
-#         if form.is_valid():
-#             message = form.save()
-#     else:
-#         form = MessageForm()
-#
-#     return render(request, 'db.html', {'form': form, 'messages': messages})
